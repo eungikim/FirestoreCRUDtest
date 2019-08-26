91bdc996bd5f0a57600ff145ed68e782b62b7122
BigDataIsVeryImportantInAi <znfks8819@gmail.com>
2019-08-26 18:39:54 +0900

    feat: read firestore data

--- app/src/main/java/kr/eungi/firestorecrudtest/MainActivity.java
+++ app/src/main/java/kr/eungi/firestorecrudtest/MainActivity.java
@@ -1,18 +1,27 @@
 package kr.eungi.firestorecrudtest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.mifmif.common.regex.Main;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @BindView(R.id.main_name_recycler_view) RecyclerView mNameRecyclerview;
     @BindView(R.id.main_add_new_floating_button) FloatingActionButton mAddNewButton;
@@ -30,7 +39,26 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "hi", Toast.LENGTH_SHORT).show();
             }
         });
-        
-        
+
+
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("name")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
+
     }
 }
