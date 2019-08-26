820429b56d89343ca9c5c736a99f9ee4bf42b621
BigDataIsVeryImportantInAi <znfks8819@gmail.com>
2019-08-26 18:09:53 +0900

    feat: make main ui layout modify sytle to no action bar and gradle

--- app/src/main/java/kr/eungi/firestorecrudtest/MainActivity.java
+++ app/src/main/java/kr/eungi/firestorecrudtest/MainActivity.java
@@ -1,14 +1,36 @@
 package kr.eungi.firestorecrudtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_name_recycler_view) RecyclerView mNameRecyclerview;
+    @BindView(R.id.main_add_new_floating_button) FloatingActionButton mAddNewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        
+        
+        mAddNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "hi", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        
     }
 }
